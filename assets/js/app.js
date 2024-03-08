3545d5e9723373cccdac66bbcb7256f43c9ad4aa
shodibek <baxtishodcoder@gmail.com>
2024-03-08 16:10:55 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,121 @@
+// FUNCTION DECLORATION
+
+console.log(func1("shodibek"));
+
+console.log(func1("ramazon"));
+
+function func1(a) {
+    return a;
+}
+console.log(func1("yusuf"));
+
+console.log(func1("aziz"));
+
+function func1(a) {
+    return a;
+}
+
+console.log(func1("nazarbek"));
+
+console.log(func1("behruz"));
+
+function func1(a) {
+    return a;
+}
+
+
+// FUNCTION EXPRESSION
+
+
+let func2 = function (n) {
+    return n;
+};
+
+console.log(func2("+998948708981"));
+
+
+let func3 = (n) => {
+    return n
+}
+console.log(func3("salom"));
+
+let func10 = function (n) {
+    return n;
+};
+
+console.log(func10("12345698758988"));
+
+
+let fun10 = (n) => {
+    return n
+}
+console.log(func3("qwertyuiop"));
+
+
+
+// ARROW FUNCTION
+// es6
+
+let func4 = (n) => {
+    return n
+}
+console.log(func4("hello world"));
+
+let func5 = (n) => {
+    return n
+}
+console.log(func4("hellouz"));
+
+
+
+// IFEE => Immidiate Invoked Function Expression
+
+(function (a) {
+
+    return console.log(func4("salomouewfhrwnfi"));
+
+
+
+}())
+
+
+// FUNCTION DECLORATION
+// let num = 123456789;
+// function func1(n, a, b, c) {
+//   //parametr
+
+//   return n + a + b + c;
+// }
+
+// console.log(func1(1, 2, 3, 4)); //argument
+
+// console.log(func1(1));
+
+// function func1 (a) {
+//     return a;
+// }
+
+// FUNCTION EXPRESSION
+// let func2 = function (n) {
+//   return n;
+// };
+
+// console.log(func2(25));
+
+// ARROW FUNCTION
+// es6
+
+// let func3 = (n) => {
+// return n
+// }
+// console.log(func3("argument"));
+
+// IFEE => Immidiate Invoked Function Expression
+
+// (function(){
+// return
+// }())
+
+
+
+
